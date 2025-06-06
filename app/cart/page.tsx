40ba04b5ce946638b51486aba8da3e8ce13d4826
roshan sahu <locroshan@gmail.com>
2025-06-06 18:27:48 +0530

    cart modified

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -3,22 +3,29 @@ import Cart from "@/components/cart/cart";
 import { cookies } from 'next/headers';
 
 async function getCart() {
-  const cookieStore =await cookies();
+  const cookieStore = await cookies(); // ✅ no await
   const token = cookieStore.get("token")?.value;
-  const response = await fetch(process.env.NEXT_PUBLIC_BACKEND_URL+"/cart",{
-    method:"GET",
-    headers: {
-      Cookie: `token=${token}`
+  if (!token) return { beats: [] };
+  try{
+
+    const response = await fetch(process.env.NEXT_PUBLIC_BACKEND_URL+"/cart",{
+      method:"GET",
+      headers: {
+        Cookie: `token=${token}`
+      }
+    })
+    if(response.ok){
+      return await response.json();
     }
-  })
-  if(response.ok){
-    return await response.json();
+    return { beats: [] };
+  }catch(err){
+    console.log(err)
   }
 }
 const page = async () => {
   const data = await getCart();
-  const beats = data.beats;
-  return (
+  const beats = data?.beats || [];
+   return (
     <>
       <div className="min-h-screen flex flex-col bg-background">
         <div className={`container px-4 py-12 mx-auto mt-3`}>
